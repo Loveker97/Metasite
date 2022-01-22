0f3e6b75d9d931abe643c8bcd5703a605b189bd9
Hang Zhang <haz006@eng.ucsd.edu>
2022-01-21 20:51:24 -0800

    Frontend initiation

--- backend/src/DjangoRestApiMongoDB/Metasite/APIs/posts_api.py
+++ backend/src/DjangoRestApiMongoDB/Metasite/APIs/posts_api.py
@@ -2,6 +2,7 @@ from django.http.response import JsonResponse
 from Metasite.serializers import PostsSerializer
 from Metasite.models import Posts
 
+from rest_framework import status
 from rest_framework.decorators import api_view
 
 @api_view(['GET', 'POST', 'DELETE'])
@@ -10,12 +11,12 @@ def posts_list(request):
         test post api
     '''
     if request.method == 'GET':
-        posts = Posts.objects.all()
+        posts = Posts.objects.filter()
         print(posts)
 
         print(f'the length of posts is {len(posts)}')
         posts_serializer = PostsSerializer(posts, many=True)
-        return JsonResponse(posts_serializer.data, safe=False)
+        return JsonResponse(posts_serializer.data, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-  
+
     
